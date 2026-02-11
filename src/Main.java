bfd6a7dde490e885346a95022a9ad3e2de254a2e
Andressa Xavier <drexavier02@gmail.com>
2026-02-11 20:34:27 -0300

    Primeiro commit - exercícios iniciais Java

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner cc = new Scanner(System.in);
+            double num1, num2, media;
+
+            for (int i = 0; i <= 3; i++) {
+
+                System.out.println("Digite a primeira nota:");
+                num1 = cc.nextDouble();
+
+                System.out.println("Digite a segunda nota:");
+                num2 = cc.nextDouble();
+
+                media = (num1 + num2) / 2;
+
+                System.out.printf("A média das nota é: %.1f\n", media);
+
+                if (media >= 7.0) {
+                    System.out.println("Parabéns!!Passou");
+                } else if (media > 4.0 && media < 6.9) {
+                    System.out.println("Vai fazer recuperação.");
+                } else {
+                    System.out.println("Reprovado!");
+                }
+            }
+        }
+    }
---
+++ src/Nota.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Nota {
+    public static void main (String[] args){
+        Scanner bb= new Scanner(System.in);
+        int [] notas = new int[5];
+        notas[0] = 6;
+        notas[1] = 9;
+        notas[2] = 3;
+        notas[3] = 1;
+        notas[4] = 8;
+
+        System.out.println(Arrays.toString(notas));
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println(notas[i]);
+        }
+
+
+    }
+}
