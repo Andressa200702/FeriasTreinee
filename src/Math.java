a15031adea59a16f0884970d67dfe2deb193dff1
Andressa Xavier <drexavier02@gmail.com>
2026-02-19 19:41:41 -0300

    Matrizes

---
+++ src/Math.java
@@ -0,0 +1,20 @@
+//Crie uma matriz 2x2 e mostre ela
+
+import java.util.Arrays;
+
+public class Math {
+     public static void main() {
+
+         int[][] matriz = {
+                 {1, 2},
+                 {3, 4}
+         };
+
+         for (int i = 0; i < 2; i++) {
+             for (int j = 0; j < 2; j++) {
+                 System.out.print(matriz[i][j] + " ");
+             }
+             System.out.println();
+         }
+     }
+}
---
+++ src/Soma.java
@@ -0,0 +1,17 @@
+//Somar as diagonais
+public class Soma {
+    static void main() {
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            soma += matriz[i][i];
+        }
+        System.out.println("Soma da diagonal: " + soma);
+    }
+}
